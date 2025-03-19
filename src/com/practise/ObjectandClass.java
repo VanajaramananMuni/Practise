111c2380f7be641074f2aef50d064cc567fdbb15
Vanajaramanan <ramananvm999@gmail.com>
2025-03-19 14:17:25 +0530

    second commit

--- src/com/practise/ObjectandClass.java
+++ src/com/practise/ObjectandClass.java
@@ -1,5 +1,9 @@
 package com.practise;
 
 public class ObjectandClass {
+	public static void main(String[] args) {
+		System.out.println(
+				"here we are going to learn what is class and object and how we are using and why we are using");
+	}
 
 }
